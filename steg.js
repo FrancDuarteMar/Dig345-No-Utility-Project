b9a44cfe01bdc0ff90b979a9d52f2a7c82bb0ea2
Francisco Duarte M <frduartemartinez@davidson.edu>
2023-02-28 13:16:13 -0500

    Added steganography tests

--- steg.js
+++ steg.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs");
 // const steg = require("./lib/steganography")
 const steg = require("./lib/selfSteg")
+const st = require("./lib/st")
 
 var base64Img = require('base64-img');
 const {
@@ -18,7 +19,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 function uploadedFiles(req, res) {
-    var encImg = steg.encode(req.body.mytext,"./public/uploads/image.png","./public/uploads/encrypt.jpg")
+    var encImg = steg.encode(req.body.mytext,base64Img.base64Sync("./public/uploads/image.png"))
     // console.log(encImg)
     res.render("home", {
         img_src: base64Img.base64Sync("./public/uploads/image.png"),
@@ -27,7 +28,18 @@ function uploadedFiles(req, res) {
     })
 }
 
-function finishedUpload(req, res) {
+function decodeFiles(req, res) {
+    var decodedText = steg.decode(base64Img.base64Sync("./public/uploads/encoded.png"))
+    console.log("Decoded text at steg.js"+decodedText)
+    // console.log(encImg)
+    res.render("home", {
+        img_enc: base64Img.base64Sync("./public/uploads/encoded.png"),
+        decoded_text: decodedText
+        
+    })
+}
+
+function encodeUpload(req, res) {
     // console.log(req.body.mytext) 
     {
         const tempPath = req.file.path
@@ -43,6 +55,77 @@ function finishedUpload(req, res) {
                 }
                 res.status(200)
                 uploadedFiles(req, res)
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handlers.serverError;
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
+    }
+
+}
+
+function encText(req, res) {
+    var encImg = st.encode(req.body.mytext,base64Img.base64Sync("./public/uploads/image.png"))
+    // console.log(encImg)
+    res.render("home", {
+        img_src: base64Img.base64Sync("./public/uploads/image.png"),
+        img_enc: encImg,
+        prompt: req.body.mytext
+    })
+}
+
+function testEncodeUpload(req, res) {
+    {
+        const tempPath = req.file.path
+        const targetPath = path.join(__dirname, "./public/uploads/image.png")
+        console.log("Uploading")
+        var extension = path.extname(req.file.originalname).toLowerCase()
+        if (extension === ".png" || extension === ".jpg" || extension === ".jpeg") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) {
+                    console.log("Error at start of upload. Unknwon error")
+                    console.log(err)
+                    return handlers.serverError;
+                }
+                res.status(200)
+                encText(req, res)
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handlers.serverError;
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
+    }
+
+}
+
+function decodeUpload(req, res) {
+    // console.log(req.body.mytext) 
+    {
+        const tempPath = req.file.path
+        const targetPath = path.join(__dirname, "./public/uploads/encoded.png")
+        console.log("Uploading")
+        var extension = path.extname(req.file.originalname).toLowerCase()
+        if (extension === ".png" || extension === ".jpg" || extension === ".jpeg") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) {
+                    console.log("Error at start of upload. Unknwon error")
+                    console.log(err)
+                    return handlers.serverError;
+                }
+                res.status(200)
+                decodeFiles(req, res)
             });
         } else {
             fs.unlink(tempPath, err => {
@@ -75,7 +158,12 @@ try {
 
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
-app.post("/upload", upload.single("file"), finishedUpload);
+app.get('/decode',handlers.decode)
+app.get('/test',handlers.steg)
+app.post("/upload", upload.single("file"), encodeUpload)
+app.post("/decode", upload.single("file"), decodeUpload)
+app.post("/uploadTest",upload.single("file"),testEncodeUpload)
+
 
 //404
 app.use(handlers.notFound)
